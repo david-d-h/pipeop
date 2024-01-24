9772188435e8ec49bdff6468bc2bc1b97d197362
David <75451291+dulkoss@users.noreply.github.com>
2024-01-24 22:14:29 +0100

    add partial invocation of pipes

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,34 @@
 #[macro_export]
 macro_rules! pipe {
-    /* If no pipes were found, throw a comptime error and stop the compilation process, invalid usage was found. */
+    // If no pipes were found, throw a comptime error and stop the compilation process, invalid usage was found.
     (@accumulate_expression [$($expr:tt)*]) => (::std::compile_error!("failed to accumulate pipeline, missing pipes."));
 
-    /* Found a pipe operator, the expression should now have been accumulated into an expression. Call the internal @accumulate_pipes rule. */
-    (@accumulate_expression [$expr:expr] |> $($tail:tt)+) => ($crate::pipe!(@accumulate_pipes [$expr] [] |> $($tail)+));
+    // Found a pipe operator, the expression should now have been accumulated into
+    // an expression. Call the internal @accumulate_pipes rule.
+    (@accumulate_expression [$expr:expr] |> $($tail:tt)+) => ($crate::pipe!(
+        @accumulate_pipes [$expr] [] |> $($tail)+
+    ));
 
-    /* Accumulate the next token into the expression buffer and recurse. */
-    (@accumulate_expression [$($expr:tt)*] $token:tt $($tail:tt)*) => ($crate::pipe!(@accumulate_expression [$($expr)* $token] $($tail)*));
+    // Accumulate the next token into the expression buffer and recurse.
+    (@accumulate_expression [$($expr:tt)*] $token:tt $($tail:tt)*) => ($crate::pipe!(
+        @accumulate_expression [$($expr)* $token] $($tail)*
+    ));
 
-    /* A pipe that consists of only an identifier, this assumes the identifier is callable. */
-    (@accumulate_pipes [$($expr:tt)*] [$($pipes:tt)*] |> $pipe:ident $($tail:tt)*) => ($crate::pipe!(@accumulate_pipes [$($expr)*] [$($pipes)* [$pipe]] $($tail)*));
+    // This arm matches a partial invocation of a pipe where `@` will be replaced by the
+    // value being passed through the pipeline.
+    (@accumulate_pipes [$expr:expr] [$($pipes:tt)*] |> $pipe:ident($($l_arg:expr,)* $(@ $(@$($_:tt)* $value:tt)? $(, $r_arg:expr)*)?) $($tail:tt)*) => ($crate::pipe!(
+        @accumulate_pipes [$expr] [$($pipes)* [
+            (|value| $pipe($($l_arg,)* $($($value)? value, $($r_arg),*)?))
+        ]] $($tail)*
+    ));
 
-    /* No more pipes were found, execute all the pipes in order with the result of the previous. Or the expression buffer if no previous piped-value exists. */
+    // This arm matches a pipe that consists of only an identifier, this assumes the identifier is callable.
+    (@accumulate_pipes [$expr:tt] [$($pipes:tt)*] |> $pipe:ident $($tail:tt)*) => ($crate::pipe!(
+        @accumulate_pipes [$expr] [$($pipes)* [$pipe]] $($tail)*
+    ));
+
+    // No more pipes were found, execute all the pipes in order with the result of the previous,
+    // or the expression buffer if no previous piped-value exists.
     (@accumulate_pipes [$expr:expr] [$([$($pipe:tt)+])+]) => ({
         let current = $expr;
 
@@ -23,9 +39,11 @@ macro_rules! pipe {
         current
     });
 
-    /* Accepts any tokens and attempts to parse them as a pipeline. */
-    ($($tokens:tt)+) => ($crate::pipe!(@accumulate_expression [] $($tokens)*));
+    // Accepts any tokens and attempts to parse them as a pipeline.
+    ($($tokens:tt)+) => ($crate::pipe!(
+        @accumulate_expression [] $($tokens)*
+    ));
 
-    /* An empty pipeline results in a unit type. */
+    // An empty pipeline results in a unit type.
     () => (());
 }
