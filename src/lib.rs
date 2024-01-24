a58260747c236008363cd2535313dff20c93fe88
David <75451291+dulkoss@users.noreply.github.com>
2024-01-24 20:55:05 +0100

    switch pipes over to an accumulation system to allow other forms of pipes, such as partial invocations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 #[macro_export]
 macro_rules! pipe {
-    (@accumulate [$($expr:tt)*]) => (::std::compile_error!("failed to accumulate pipeline, missing pipes."));
-    (@accumulate [$($expr:tt)*] |> $($tail:tt)+) => ($crate::pipe!([$($expr)*] |> $($tail)+));
-    (@accumulate [$($expr:tt)*] $token:tt $($tail:tt)*) => ($crate::pipe!(@accumulate [$($expr)* $token] $($tail)*));
-    ([$expr:expr] $(|> $ident:ident)+) => ({
+    (@accumulate_expression [$($expr:tt)*]) => (::std::compile_error!("failed to accumulate pipeline, missing pipes."));
+    (@accumulate_expression [$($expr:tt)*] |> $($tail:tt)+) => ($crate::pipe!(@accumulate_pipes [$($expr)*] [] |> $($tail)+));
+    (@accumulate_expression [$($expr:tt)*] $token:tt $($tail:tt)*) => ($crate::pipe!(@accumulate_expression [$($expr)* $token] $($tail)*));
+    (@accumulate_pipes [$($expr:tt)*] [$($pipes:tt)*] |> $pipe:ident $($tail:tt)*) => ($crate::pipe!(@accumulate_pipes [$($expr)*] [$($pipes)* [$pipe]] $($tail)*));
+    (@accumulate_pipes [$expr:expr] [$([$($pipe:tt)+])+]) => ({
         let current = $expr;
 
         $(
-            let current = $ident(current);
+            let current = $($pipe)*(current);
         )+
 
         current
     });
-    ($($tokens:tt)*) => ($crate::pipe!(@accumulate [] $($tokens)*));
+    ($($tokens:tt)*) => ($crate::pipe!(@accumulate_expression [] $($tokens)*));
 }
