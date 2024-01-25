5c3538bccc0affd61e49974c5629a1d7aa295826
David <75451291+dulkoss@users.noreply.github.com>
2024-01-25 18:52:41 +0100

    allow pipeline to be stored in a closure by using `...` as an expression

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-
 #[macro_export]
 macro_rules! pipe {
     // If no pipes were found, throw a comptime error and stop the compilation process, invalid usage was found.
@@ -7,8 +6,8 @@ macro_rules! pipe {
 
     // Found a pipe operator, the expression should now have been accumulated into
     // an expression. Call the internal @accumulate_pipes rule.
-    (@accumulate_expression [$expr:expr] |> $($tail:tt)+) => ($crate::pipe!(
-        @accumulate_pipes [$expr] [] |> $($tail)+
+    (@accumulate_expression [$($expr:tt)+] |> $($tail:tt)+) => ($crate::pipe!(
+        @accumulate_pipes [$($expr)+] [] |> $($tail)+
     ));
 
     // Accumulate the next token into the expression buffer and recurse.
@@ -19,52 +18,57 @@ macro_rules! pipe {
     // This arm matches a partial invocation of a pipe where `@` will be replaced by the
     // value being passed through the pipeline.
     (
-        @accumulate_pipes [$expr:expr] [$($pipes:tt)*]
+        @accumulate_pipes [$($expr:tt)+] [$($pipes:tt)*]
         |> $(:: $(@$($_:tt)* $prefixed:tt)?)? $ident:ident $(:: $path:ident)*
         ($($l_arg:expr,)* $(@ $(@$($__:tt)* $value:tt)? $(, $r_arg:expr)*)?)
         $($tail:tt)*
     ) => ($crate::pipe!(
-        @accumulate_pipes [$expr] [$($pipes)*
+        @accumulate_pipes [$($expr)+] [$($pipes)*
             [|value| $($($prefixed)? ::)? $ident $(:: $path)* ($($l_arg,)* $($($value)? value, $($r_arg),*)?)]
         ] $($tail)*
     ));
 
     // This arm matches a pipe that consists of only an identifier, this assumes the identifier is callable.
     (
-        @accumulate_pipes [$expr:tt] [$($pipes:tt)*]
+        @accumulate_pipes [$($expr:tt)+] [$($pipes:tt)*]
         |> $(:: $(@$($_:tt)* $prefixed:tt)?)? $ident:ident $(:: $path:ident)*
         $(|> $($tail:tt)*)?
     ) => ($crate::pipe!(
-        @accumulate_pipes [$expr]
+        @accumulate_pipes [$($expr)+]
             [$($pipes)* [$($($prefixed)? ::)? $ident $(:: $path)*]
         ] $(|> $($tail)*)?
     ));
 
     // This arm matches a method invocation on the value currently going through the pipeline.
-    (@accumulate_pipes [$expr:expr] [$($pipes:tt)*] |> . $pipe:ident($($arg:expr),*) $($tail:tt)*) => ($crate::pipe!(
-        @accumulate_pipes [$expr] [$($pipes)* [|value| value.$pipe($($($arg),*)?)]] $($tail)*
+    (@accumulate_pipes [$($expr:tt)+] [$($pipes:tt)*] |> . $pipe:ident($($arg:expr),*) $($tail:tt)*) => ($crate::pipe!(
+        @accumulate_pipes [$($expr)+] [$($pipes)* [|value| value.$pipe($($($arg),*)?)]] $($tail)*
     ));
 
     // This arm matches a method invocation without parentheses, and thus also without arguments.
-    (@accumulate_pipes [$expr:expr] [$($pipes:tt)*] |> . $pipe:ident $($tail:tt)*) => ($crate::pipe!(
-        @accumulate_pipes [$expr] [$($pipes)* [|value| value.$pipe()]] $($tail)*
+    (@accumulate_pipes [$($expr:tt)+] [$($pipes:tt)*] |> . $pipe:ident $($tail:tt)*) => ($crate::pipe!(
+        @accumulate_pipes [$($expr)+] [$($pipes)* [|value| value.$pipe()]] $($tail)*
     ));
 
     // This arm matches a closure with a block.
-    (@accumulate_pipes [$expr:expr] [$($pipes:tt)*] |> |$ident:ident| $block:block $($tail:tt)*) => ($crate::pipe!(
-        @accumulate_pipes [$expr] [$($pipes)* [|$ident| $block]] $($tail)*
+    (@accumulate_pipes [$($expr:tt)+] [$($pipes:tt)*] |> |$ident:ident| $block:block $($tail:tt)*) => ($crate::pipe!(
+        @accumulate_pipes [$($expr)+] [$($pipes)* [|$ident| $block]] $($tail)*
     ));
 
     // This arm matches a closure without a block.
-    (@accumulate_pipes [$expr:expr] [$($pipes:tt)*] |> |$ident:ident| $($block:tt)+ $(|> $($tail:tt)*)?) => ($crate::pipe!(
-        @accumulate_pipes [$expr] [$($pipes)* [|$ident| $($block)+]] $(|> $($tail)*)?
+    (@accumulate_pipes [$($expr:tt)+] [$($pipes:tt)*] |> |$ident:ident| $($block:tt)+ $(|> $($tail:tt)*)?) => ($crate::pipe!(
+        @accumulate_pipes [$($expr)+] [$($pipes)* [|$ident| $($block)+]] $(|> $($tail)*)?
+    ));
+
+    // Create a closure that encapsulates the pipeline, so the pipeline can be reused.
+    (@accumulate_pipes [...] [$($pipes:tt)+]) => ((
+        |item| $crate::pipe!(@accumulate_pipes [item] [$($pipes)+])
     ));
 
     // No more pipes were found, execute all the pipes in order with the result of the previous,
     // or the expression buffer if no previous piped-value exists and return the result.
     (@accumulate_pipes [$expr:expr] [$([$($pipe:tt)+])+]) => ({
         let current = $expr;
-        $(let current = $crate::call_with($($pipe)*, current);)+
+        $(let current = $crate::call_with($($pipe)+, current);)+
         current
     });
 
